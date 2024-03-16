55a5524c42949d7d10a96f8c9917d66b1c9fe7fd
Zlata <zlata_839@mail.ru>
2024-03-16 23:50:55 +0600

    add task2

---
+++ Seminars/task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает целое число N, 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+int N = Convert.ToInt32(Console.ReadLine());
+int notN = -N;
+
+while (notN <= N)
+{
+    Console.Write($"{notN} ");
+    notN++;
+}
