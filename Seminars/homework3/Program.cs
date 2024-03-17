2c39979e7efc5c8ade772f0219f19b30f9051d2b
Zlata <zlata_839@mail.ru>
2024-03-18 00:30:08 +0600

    add homework3

---
+++ Seminars/homework3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на ввод принимает число и выдает, является ли число четным.
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числв от 1 до N
+
+int number;
+number = Console.Read();
+if ((number % 2) == 0)
+{
+    Console.WriteLine("четное");
+}
+else
+{
+    Console.WriteLine("нечетное");
+}
+
+  int x = Convert.ToInt32(Console.ReadLine());
+        if ((x % 2) == 0)
+        {
+            Console.WriteLine("chentoe");
+        }
+        else
+        {
+            Console.WriteLine("nechentoe");
+        }
