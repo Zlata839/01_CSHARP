a48d5c601083549ed6959ca82a5dcafe4a5de325
Zlata <zlata_839@mail.ru>
2024-03-17 22:50:26 +0600

    add seminar1:homework1

---
+++ Seminars/homework1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на ввод принемает два числа и выдвет какое число большее, а какое меньшее.
+
+int firstNumber = 5;
+int secondNumber = 6;
+
+if (firstNumber == secondNumber)
+{
+       Console.WriteLine("числа равны");
+}
+else if (firstNumber > secondNumber)
+{
+    Console.WriteLine("первое число больше");
+}
+else
+{
+    Console.WriteLine("второе число больше");
+}
