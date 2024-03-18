5bdf86209b1f6bc290ee7ccfd96b615dd24b1467
Zlata <zlata_839@mail.ru>
2024-03-19 00:44:51 +0600

    add task4

---
+++ Seminar2/Task4/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая выводит третью слева цифру заданного 
+// числа или сообщает, что третьей цифры нет.
+
+// 456  =>  6
+// 7812  =>  1
+// 91  =>  Третьей цифры нет
+
+int num = new Random().Next(0, 10000);
+Console.WriteLine(num);
+int res = 0;
+if (num < 100)
+    while (num > 999 && num < 1001)
+    {
+        Console.WriteLine($"Третьей цифры нет:{num}");
+    }
+
+else
+    {
+        while (num >= 100)
+        {
+            res = num % 10;
+            num /= 10; // num = num / 10;
+            Console.WriteLine($"num:{num}");
+            Console.WriteLine($"ответ:{res}");
+            Console.WriteLine();
+        }
+
+    }
+
+
+
+
