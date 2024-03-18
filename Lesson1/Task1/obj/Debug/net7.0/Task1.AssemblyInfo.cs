110e7bab982c9a08007801f8583839cffda4a79f
Zlata <zlata_839@mail.ru>
2024-03-18 23:04:28 +0600

    rename folder seminars:seminar1

--- Lesson1/Task1/obj/Debug/net7.0/Task1.AssemblyInfo.cs
+++ Lesson1/Task1/obj/Debug/net7.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+becda7bf1ec0586af8653e957f98e7a4df8163c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
