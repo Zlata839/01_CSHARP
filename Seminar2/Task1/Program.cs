5f46efbc0a838c169f93edcaaa7ef1925c72f921
Zlata <zlata_839@mail.ru>
2024-03-18 23:32:14 +0600

    add task1

---
+++ Seminar2/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход  трёхзначное число и 
+// удаляет вторую цифру этого числа.
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+int num_1 = num % 10;
+int num_2 = num / 100;
+
+// Console.WriteLine(num_2 + "" + num_1);
+// Console.WriteLine(num_2 * 10 + num_1);
+
+int result = num_2 * 10 + num_1;
+Console.WriteLine($"{num} -> {result}");
+
+int result_2 = Convert.ToInt32((num_2 + "" + num_1));
+Console.WriteLine($"{num} -> {result_2}");
+
+string num_str = Convert.ToString(num);
+Console.WriteLine(Convert.ToString(num_str[0] + Convert.ToString(num_str[2])));
+Console.WriteLine(num_str[0] + "" + num_str[2]);
