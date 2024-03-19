e8e029d6ab7fd7f281a4271ba45354050a6e3f57
Zlata <zlata_839@mail.ru>
2024-03-20 00:22:30 +0600

    add task2

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while (i < n)
+{
+    arr[i] = i + 1;
+    // Console.WriteLine(arr[i]);
+    // Console.Write(' ');
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
