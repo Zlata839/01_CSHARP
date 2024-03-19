17c4424ddcd485e206af763d0eef01fa420c12f9
Zlata <zlata_839@mail.ru>
2024-03-20 00:51:28 +0600

    add task4

---
+++ Lesson2/Task4/Program.cs
@@ -0,0 +1,34 @@
+﻿int n = 5;
+int[] array = { 10, 4, 7, 3, 9 };
+int i = 0;
+int max = array[0];
+
+while (i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
+
+max = array[0];
+for (int j = 0; j < n; j++)
+{
+    if(array[j] > max)
+    {
+        max = array[j];
+    }
+}
+Console.WriteLine(max);
+
+max = array[0];
+foreach(int e in array)
+{
+    if(e > max)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);
