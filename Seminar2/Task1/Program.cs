73fe4c15ddecfedb092e44b90523cfcffa96fafd
Zlata <zlata_839@mail.ru>
2024-03-19 20:36:34 +0600

    add comments

--- Seminar2/Task1/Program.cs
+++ Seminar2/Task1/Program.cs
@@ -6,8 +6,10 @@ Console.WriteLine(num);
 int num_1 = num % 10;
 int num_2 = num / 100;
 
-// Console.WriteLine(num_2 + "" + num_1);
-// Console.WriteLine(num_2 * 10 + num_1);
+// Console.WriteLine(num_2 + "" + num_1); склейка без пробела
+// Console.WriteLine(num_2 * 10 + num_1); 
+// * 10 это что num_2 стал числом из 2 цифр и вторым числом будет num_1
+// например 223 стал num_2 - 2,23 а когда * 10 стало 20 вместо 0 подставится num_1
 
 int result = num_2 * 10 + num_1;
 Console.WriteLine($"{num} -> {result}");
@@ -17,4 +19,6 @@ Console.WriteLine($"{num} -> {result_2}");
 
 string num_str = Convert.ToString(num);
 Console.WriteLine(Convert.ToString(num_str[0] + Convert.ToString(num_str[2])));
-Console.WriteLine(num_str[0] + "" + num_str[2]);
+// Добавили Convert.ToString потому что иначе C# считает как тип char
+Console.WriteLine(num_str[0] + "" + num_str[2]); 
+// Без суммирования тогда склейка
--- Seminar2/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Seminar2/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+110e7bab982c9a08007801f8583839cffda4a79f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83dc49761f47e0ea19c6a057bbf898bfbc0ad074")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
