0cbbefe80e70d93f6753aad3e7772a4ccffd99f2
Zlata <zlata_839@mail.ru>
2024-03-21 19:30:31 +0600

    add hometask4:seminar2

---
+++ Seminar2/Hometask4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N, 
+// а на выходе показывает его цифры через запятую.
+
+Console.Write("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 10)
+{
+    Console.Write($"Число {N} состоит из одной цифры");
+}
+else
+{
+    while (N > 0)
+    {
+        int res = N % 10;
+        N /= 10;
+        if (N > 0)
+        {
+            Console.Write(res + ", ");
+        }
+        else
+        {
+            Console.WriteLine(res);
+        }
+    }
+}
