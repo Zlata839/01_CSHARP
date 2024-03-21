ff0c14464b5a3f409a1eed37c5f6167740fab5c8
Zlata <zlata_839@mail.ru>
2024-03-22 00:45:00 +0600

    add task3:seminar3

--- Seminar3/Task3/Program.cs
+++ Seminar3/Task3/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] Append(int len)
+{
+    int[] list = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        list[i] = new Random().Next(-100, 101);
+    }
+    foreach (int i in list)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+    return list;
+}
+
+var len = 6;
+
+var lst = Append(len);
+
+var l = lst.Length;
+
+int index = 0;
+
+
+while (index < lst.Length / 2)
+{
+    Console.Write(lst[index] * lst[lst.Length - index - 1] + " ");
+
+    index++;
+}
