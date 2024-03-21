ef6624a9916aa29184bbe6928568740d26f84d46
Zlata <zlata_839@mail.ru>
2024-03-22 01:19:27 +0600

    add task4:seminar3

---
+++ Seminar3/Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Дано натуральное трёхзначное число. Создайте массив, 
+// состоящий из цифр этого числа. Младший разряд числа должен 
+// располагаться на 0-м индексе массива, старший – на 2-м.
+
+void PrintMas(int[] col)
+{
+    foreach (int item in col)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
+
+string n = Console.ReadLine();
+int len = n.Length;
+
+int[] arr = new int[len];
+for (int i = 0; i < len; i++)
+{
+    arr[i] = Convert.ToInt32(Convert.ToString(n[i]));
+}
+PrintMas(arr);
+
+int[] arr2 = new int[len];
+for (int i = 0; i < len; i++)
+{
+    arr2[i] = Convert.ToInt32(Convert.ToString(n[len - 1 - i]));
+}
+PrintMas(arr2);
