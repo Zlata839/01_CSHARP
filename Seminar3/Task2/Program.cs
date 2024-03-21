3161107b8209f5b8f06f798c23810dab3f032e40
Zlata <zlata_839@mail.ru>
2024-03-22 00:29:57 +0600

    add task3:seminar3

---
+++ Seminar3/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте массив из 10 элементов, заполненный числами из промежутка 
+// [-10, 10]. Замените отрицательные элементы на положительные, 
+// а положительные на отрицательные.
+
+void PrintMas(int[] col)
+{
+    foreach (int item in col)
+    {
+        Console.Write($"{item}\t ");
+    }
+    Console.WriteLine();
+}
+
+int[] RandomMas(int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        col[i] = new Random().Next(-10, 11);
+    }
+    return col;
+}
+
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = RandomMas(new int[n]);
+PrintMas(arr);
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] *= -1;
+}
+PrintMas(arr);
