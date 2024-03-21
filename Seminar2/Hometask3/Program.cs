beb74bf43ee8236a8a279cea04305eb801f5a10e
Zlata <zlata_839@mail.ru>
2024-03-21 18:22:04 +0600

    add hometask3:seminar2

---
+++ Seminar2/Hometask3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+
+int n = new Random().Next(11, 100);
+// int n = 55;
+
+int firstNumber = n / 10;
+int secondNumber = n % 10;
+
+int max = 0;
+
+if (firstNumber > secondNumber)
+{
+    max = firstNumber;
+    Console.Write($"{max} Наибольшая цифра числа {n}");
+}
+else if (firstNumber == secondNumber)
+{
+    Console.Write($"Обе цифры числа {n} равны");
+}
+else
+{
+    max = secondNumber;
+    Console.Write($"{max} Наибольшая цифра числа {n}");
+}
