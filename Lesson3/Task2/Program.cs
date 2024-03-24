ff65c6804734302a898c01d6669fbe5daa9331bb
Zlata <zlata_839@mail.ru>
2024-03-24 14:59:03 +0600

    Add task2:lesson3

---
+++ Lesson3/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿void PrintSquares (int limit)
+{
+    int i = 1;
+    while(i <= limit)
+    {
+        Console.Write($"{i*i} "); // возводим в квадрат
+        i++;
+    }
+}
+
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
