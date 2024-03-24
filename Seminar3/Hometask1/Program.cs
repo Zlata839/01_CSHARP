1d5637d44d0797ab10f47b54981b140fb9e3db94
Zlata <zlata_839@mail.ru>
2024-03-24 19:06:51 +0600

    Hometask1:seminar3

---
+++ Seminar3/Hometask1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
+{
+    // Подсчет количества элементов массива, попадающих в заданный диапазон
+    // numbers - массив, в котором ведется подсчет
+    // minRange - минимальная граница диапазона
+    // maxRange - максимальная граница диапазона
+    public static int CountItemsRange(int[] numbers, int minRange, int maxRange)
+    {
+        //Введите сюда свое решение
+        int count = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] >= minRange && numbers[i] <= maxRange)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void PrintResult(int[] array)
+    {
+
+        //Введите сюда свое решение
+        Console.WriteLine(CountItemsRange(array, 10, 90));
+    }
+
+}
+
+
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        int[] array;
+
+
+        if (args.Length >= 1)
+        {
+            // Объединяем все аргументы командной строки в одну строку
+            string joinedArgs = string.Join(" ", args);
+
+            // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+            array = joinedArgs.Split(", ")
+                                  .Select(int.Parse)
+                                  .ToArray();
+
+            // Теперь arr содержит преобразованные в целые числа из командной строки
+
+        }
+        else
+        {
+            // Если аргументов на входе нет
+            array = new int[] { 1, 5, 10, 20, 30, 40, 99, 4, 90, 3 }; // Создание массива
+        }
+        UserInputToCompileForTest.PrintResult(array);
+    }
+}
