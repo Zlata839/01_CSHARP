57ebf755846cd5dff760109617a70f81cc3eb3bb
Zlata <zlata_839@mail.ru>
2024-03-28 19:32:24 +0600

    add task3:lesson4

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Считать с консоли строку, состоящую из цифр и латинских букв. 
+// Сформировать новую строку, состоящую из букв исходной строки 
+
+string GetlettersFromString(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = GetlettersFromString(str);
+Console.WriteLine(result);
