7d530284103d17caf3c85b083da0e64324d9eb04
Zlata <zlata_839@mail.ru>
2024-03-28 17:46:42 +0600

    add task3:lesson4

---
+++ Lesson4/Task2/Program.cs
@@ -0,0 +1,62 @@
+﻿// Назовем число "интересным" если его сумма цифр четная. 
+// Создать двумерный массв, состоящий из целых чисел. 
+// Вывести на экран "интересные" элементы массива
+
+int[,] CreateMatrix(int rowCount, int columsCount)
+{
+    int[,] matrix = new int[rowCount, columsCount];
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 1000);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(3, 4);
+ShowMatrix(matrix);
+
+foreach (int e in matrix)
+{
+    if (isinteresting(e) == true)
+    {
+        Console.WriteLine(e);
+    }
+}
+
+bool isinteresting(int value)
+{
+    int sumOfDigits = GetSumOfDigits(value);
+    if (sumOfDigits % 2 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+int GetSumOfDigits(int value)
+{
+    int sum = 0;
+    while (value > 0)
+    {
+        sum = sum + value % 10;
+        value = value / 10;
+    }
+    return sum;
+}
