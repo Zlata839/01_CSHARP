5fbc5874f5216f5e0e723e6890c59add2d76cd8b
Zlata <zlata_839@mail.ru>
2024-04-02 00:17:05 +0600

    add task4:seminar6

---
+++ Seminar6/Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Считать строку с консоли, состоящую из латинских букв в нижнем регистре. 
+// Выяснить, сколько среди введённых букв гласных (aouei).
+
+using System;
+using System.Text;
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+int FindVowels(string str)
+{
+    string vowels = "aoueiуеаояиыюэAOUEIУЕЫАОЭЯИЮ";
+    int count = 0;
+    for (int i = 0; i < str.Length; i++)
+    { 
+       for (int j = 0; j < vowels.Length; j++) 
+       {
+        if (str[i] == vowels[j])
+        {
+            count++;
+            break;
+        }
+       }
+    }
+    return count;
+}
+
+Console.Write("Введите строку: ");
+string n = Console.ReadLine()!;
+int res = FindVowels(n);
+Console.WriteLine(res);
+
+
+
