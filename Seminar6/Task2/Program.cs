38b823ba444c5a0c335f883b6a3e61a47f81b4f1
Zlata <zlata_839@mail.ru>
2024-04-01 23:42:10 +0600

    add task2:seminar6

---
+++ Seminar6/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// На основе символов строки (тип string) сформировать массив символов (тип char[]). Вывести массив на экран.
+// “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ]
+
+using System;
+// расскладка кириллица
+using System.Text;
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+void PrintMass(char[] collection)
+{
+    foreach (var item in collection)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите символы: ");
+string n = Console.ReadLine()!;
+char[] mass = new char[n.Length];
+for (int i = 0; i < n.Length; i++)
+{
+    mass[i] = n[i];
+}
+
+PrintMass(mass);
+
