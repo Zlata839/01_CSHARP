36bc6f43bcaf519b2c17a6adf222fe68647985d8
Zlata <zlata_839@mail.ru>
2024-04-03 02:19:56 +0600

    add task 1:lesson5

---
+++ Lesson5/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вычеслить фаториал от натурального числа N
+
+int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        //отладочный вывод на ээкран
+        Console.WriteLine($"Stop: {n}");
+        return 1;
+    }
+    Console.WriteLine(n);
+    return n * Fact(n - 1); // нельзя стаивть !, так как в C# это совсем другое значение
+}
+
+Console.Write(Fact(5));
