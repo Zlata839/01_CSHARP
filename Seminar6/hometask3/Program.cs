30f748e3293d8c1a21256cb652352ed70dc7f57c
Zlata <zlata_839@mail.ru>
2024-04-04 20:16:43 +0600

    add hometask3:seminar6

---
+++ Seminar6/hometask3/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом. 
+// (Справа на лево и слева на право читается одинаково).
+
+using System.Text;
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+bool IsPalindrom(string str)
+{
+    bool a = false;
+    bool b = false;
+    int count = 0;
+    for (int i = 0; i < str.Length / 2; i++)
+    {
+        if (str[i] == str[str.Length - i - 1])
+        {
+            a = true;
+            if (a)
+            {
+                count++;
+            }
+        }
+    }
+    if (count == str.Length / 2)
+    {
+        b = true;
+    }
+    return b;
+}
+
+void Print(bool str)
+{
+    if (str)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+
+string str = "шалаш";
+bool isPalindrom = IsPalindrom(str);
+Print(isPalindrom);
+
