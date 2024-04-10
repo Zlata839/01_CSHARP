6a241f47bebc21b6a0b55c89af1c72549ccaea55
Zlata <zlata_839@mail.ru>
2024-04-11 01:25:04 +0600

    add hometask3

---
+++ Seminar7/hometask3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+void Reverse (int[] array, int i)
+{
+    if(i < 0) return;
+    Console.Write(array[i] + " ");
+    Reverse(array, i - 1);
+}
+
+int[] array = {34, 5, 67, 32, 0};
+Reverse(array, array.Length - 1);
